20677e8503eb0f540c824507ecbf5cfc7adba633
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2023-11-24 20:08:33 +0100

    declarado_llamada_eventos_delegados

--- Assets/Scripts/Events/EventsGameManager.cs
+++ Assets/Scripts/Events/EventsGameManager.cs
@@ -3,4 +3,37 @@ using UnityEngine;
 public class EventsGameManager : MonoBehaviour
 {
     // HERE YOU WILL HAVE TO DECLARE AND INVOKE THE EVENTS
+
+    public delegate void Delegado1();
+    public delegate void Delegado2();
+
+    public static event Delegado1 OnEnterKeyPressed;
+    public static event Delegado2 OnEscapeKeyPressed;
+
+    private void Update()
+    {
+
+    }
+
+    private void EnterKeyClicked()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (OnEnterKeyPressed != null)
+            {
+                OnEnterKeyPressed();
+            }
+        }
+    }
+
+    private void EscapeKeyClicked()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (OnEscapeKeyPressed != null)
+            {
+                OnEscapeKeyPressed();
+            }
+        }
+    }
 }
